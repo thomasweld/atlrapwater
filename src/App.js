17b6943016705b0b106e9d9dc9efd3c4103f01d3
Thomas Weld <thomas.weld@gmail.com>
2019-04-27 20:10:22 -0400

    back up progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Iframe from "react-iframe";
 
-import logo from "./rapwater2.0_square-01_web.jpg";
+import logo from "./atl-rap-water_logo.png";
+import footer from "./Water-Image1.jpg";
 import "./App.css";
 
 function App() {
@@ -25,23 +26,9 @@ function App() {
           />
         </div>
       </div>
+      <img src={footer} className="footer" alt="footer" />
     </div>
   );
 }
 
 export default App;
-
-// <header className="App-header">
-// <img src={logo} className="App-logo" alt="logo" />
-// <p>
-// Edit <code>src/App.js</code> and save to reload.
-// </p>
-// <a
-// className="App-link"
-// href="https://reactjs.org"
-// target="_blank"
-// rel="noopener noreferrer"
-// >
-// Learn React
-// </a>
-// </header>
